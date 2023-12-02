92cb51597fe9e3bdb3716297d691c6631dc6385f
eunsu14 <7266706@naver.com>
2023-12-02 10:01:19 +0900

    add: User 도메인

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,105 +1,75 @@
 package model;
 
-/**
- * 사용자 관리를 위해 필요한 도메인 클래스. USERINFO 테이블과 대응됨
- */
 public class User {
-	private String userId;
-	private String password;
-	private String name;
-	private String email;
-	private String phone;
-	private int commId;
-	private String commName;
+    private String userId;      
+    private String password;   
+    private String name;       
+    private String nickname;    
+    private String email;          
+    private int itLevel;
+    
+    public User() {
+        super();
+    }
 
-	public User() { }		// 기본 생성자
-	
-	public User(String userId, String password, String name, String email, String phone, int commId) {
-		this.userId = userId;
-		this.password = password;
-		this.name = name;
-		this.email = email;
-		this.phone = phone;
-		this.commId = commId;
-	}
-	
-	public User(String userId, String password, String name, String email, String phone, int commId, String commName) {
-		this(userId, password, name, email, phone, commId);
-		this.commName = commName;
-	}
+    public User(String userId, String password, String name, String nickname, String email) {
+        super();
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.nickname = nickname;
+        this.email = email;
+    }
 
-	public User(String userId, String name, String email, String phone) {
-		this.userId = userId;
-		this.name = name;
-		this.email = email;
-		this.phone = phone;		
-	}
-	
-	/*public void update(User updateUser) {
-        this.password = updateUser.password;
-        this.name = updateUser.name;
-        this.email = updateUser.email;
-        this.phone = updateUser.phone;
-    }*/
-	
-	public String getUserId() {
-		return userId;
-	}
+    public String getUserId() {
+        return userId;
+    }
 
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getNickname() {
+        return nickname;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 
-	public String getPhone() {
-		return phone;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public int getCommId() {
-		return commId;
-	}
+    public int getLevelId() {
+        return itLevel;
+    }
 
-	public void setCommId(int commId) {
-		this.commId = commId;
-	}
+    public void setLevelId(int itLevel) {
+        this.itLevel = itLevel;
+    }
 
-	public String getCommName() {
-		return commName;
-	}
-
-	public void setCommName(String commName) {
-		this.commName = commName;
-	}
-
-	
-	/* 비밀번호 검사 */
+    /* 비밀번호 검사 */
 	public boolean matchPassword(String password) {
 		if (password == null) {
 			return false;
@@ -110,10 +80,10 @@ public class User {
 	public boolean isSameUser(String userid) {
         return this.userId.equals(userid);
     }
-
-	@Override
-	public String toString() {
-		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + ", phone="
-				+ phone + ", commId=" + commId + "]";
-	}	
+	
+    @Override
+    public String toString() {
+        return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", nickname=" + nickname
+                + ", email=" + email + ", itLevel=" + itLevel + "]";
+    }
 }
