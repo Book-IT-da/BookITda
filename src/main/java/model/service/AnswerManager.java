5c091ab3eecfb62cb62d30b0c3b0648b28ca0789
eunsu14 <7266706@naver.com>
2023-12-03 14:54:27 +0900

    add: 답변 리스트

--- src/main/java/model/service/AnswerManager.java
+++ src/main/java/model/service/AnswerManager.java
@@ -22,6 +22,11 @@ public class AnswerManager {
 		return ansMan;
 	}
 	
+	// 답변 목록
+	public List<Answer> findAnswerList(int qId) throws SQLException {
+	    return answerDAO.findAnswerList(qId);
+	}
+	
 	// 마이페이지 - 사용자가 작성한 모든 답변
 	public List<Answer> findAnswerByUser(String userId) throws SQLException {
 		return answerDAO.findAnswerByUser(userId);
