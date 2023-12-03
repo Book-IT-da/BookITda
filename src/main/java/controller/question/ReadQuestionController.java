a049eab3604d423acacf89942751fdf1cfb95dc7
eunsu14 <7266706@naver.com>
2023-12-03 14:55:13 +0900

    add: 답변 리스트 관련 내용

--- src/main/java/controller/question/ReadQuestionController.java
+++ src/main/java/controller/question/ReadQuestionController.java
@@ -1,22 +1,33 @@
 package controller.question;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import controller.Controller;
+import model.Answer;
 import model.Question;
+import model.service.AnswerManager;
 import model.service.QuestionManager;
 
 public class ReadQuestionController implements Controller {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		QuestionManager quesMan = QuestionManager.getInstance();
+		AnswerManager ansMan = AnswerManager.getInstance();
+		
 		String value = request.getParameter("qId");
 	
 		Question ques = null;
+		List<Answer> answerList = null;
+		
 		try {
 			int qId = Integer.parseInt(value);
 			ques = quesMan.read(qId);
+			answerList = ansMan.findAnswerList(qId);
+		    request.setAttribute("answerList", answerList);
+			
 		} catch (Exception e) {
 			return "redirect:/question/list";
 		}
