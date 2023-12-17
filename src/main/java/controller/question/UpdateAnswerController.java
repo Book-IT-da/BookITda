034def7bbdffd9c26cd82bf5c889e168d7985f29
eunsu14 <7266706@naver.com>
2023-12-17 23:36:27 +0900

    add: UpdateAnswerController 생성

--- src/main/java/controller/question/UpdateAnswerController.java
+++ src/main/java/controller/question/UpdateAnswerController.java
@@ -2,14 +2,34 @@ package controller.question;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import controller.Controller;
+import model.Answer;
+import model.service.AnswerManager;
 
-/*
 public class UpdateAnswerController implements Controller {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String aId = request.getParameter("aId");
+        String qId = request.getParameter("qId");
+        String userId = request.getParameter("userId");
+        
+        HttpSession session = request.getSession();
+        session.setAttribute("qId", qId);
+        
+        Answer updateAnswer = new Answer(
+                Integer.parseInt(aId),
+                request.getParameter("answer"),
+                Integer.parseInt(qId),
+                userId, 
+                null);
+                
+        AnswerManager quesMan = AnswerManager.getInstance();
+        quesMan.update(updateAnswer);
+        
+        return "redirect:/question/view";
         
     }
 }
-*/
+
