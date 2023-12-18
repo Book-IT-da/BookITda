ae8942b41fe5dcce75d71e669e6d86b109f4ac45
kimyujin-com <80165026+kimyujin-com@users.noreply.github.com>
2023-12-18 14:21:04 +0900

    add: findInterestById myBatis 적용

--- src/main/java/model/service/InterestManager.java
+++ src/main/java/model/service/InterestManager.java
@@ -1,27 +1,21 @@
 package model.service;
 
 import java.sql.SQLException;
-import java.util.List;
 
-import model.Category;
 import model.Interest;
 import model.dao.jdbc.InterestDAO;
+import model.dao.mybatis.MyBatisInterestDAO;
 
 
-/**
- * 사용자 관리 API를 사용하는 개발자들이 직접 접근하게 되는 클래스.
- * UserDAO를 이용하여 데이터베이스에 데이터 조작 작업이 가능하도록 하며,
- * 데이터베이스의 데이터들을 이용하여 비지니스 로직을 수행하는 역할을 한다.
- * 비지니스 로직이 복잡한 경우에는 비지니스 로직만을 전담하는 클래스를 
- * 별도로 둘 수 있다.
- */
 public class InterestManager {
     private static InterestManager interestManager = new InterestManager();
     private InterestDAO interestDAO;
+    private MyBatisInterestDAO myBatisInterestDAO;
 
     public InterestManager() {
         try {
             interestDAO = new InterestDAO();
+            myBatisInterestDAO = new MyBatisInterestDAO();
         } catch (Exception e) {
             e.printStackTrace();
         }           
@@ -44,6 +38,6 @@ public class InterestManager {
        interestDAO.modifyUserLevel(userId, userInterest);
    }
    public Interest findInterestById(String userId) throws SQLException {
-       return interestDAO.findInterestById(userId);
+       return myBatisInterestDAO.findInterestById(userId);
    }
 }
