70195fd2d4f6beb50421c727125473a342078136
kimyujin-com <80165026+kimyujin-com@users.noreply.github.com>
2023-12-22 13:43:02 +0900

    update: myBatis 주석 삭제

--- src/main/java/model/dao/mybatis/mapper/InterestMapper.java
+++ src/main/java/model/dao/mybatis/mapper/InterestMapper.java
@@ -3,16 +3,5 @@ package model.dao.mybatis.mapper;
 import model.Interest;
 
 public interface InterestMapper {
-
-    /*
-     * public void createUserInterest(String userId, Interest userInterest);
-     * 
-     * public void modifyInterestCat(String userId, Interest userInterest);
-     * 
-     * public void modifyInterestLang(String userId, Interest userInterest);
-     * 
-     * public void modifyUserLevel(String userId, Interest userInterest);
-     */
-
     public Interest findInterestById(String userId);
 }
